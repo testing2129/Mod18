7ae3cfe724687ea34be86fd04d220dc7761583ae
Testing212 <gabriel.janvrin@gmail.com>
2025-06-03 20:43:18 +0200

    (Beta)

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -5,41 +5,51 @@ function playAudio(path) {
 }
 
 function makeSound(key) {
-    switch (key) {
-      case "w":
-        playAudio("sounds/tom-1.mp3");
-        break;
-      case "a":
-        playAudio("sounds/tom-2.mp3");
-        break;
-      case "s":
-        playAudio("sounds/tom-3.mp3");
-        break;
-      case "d":
-        playAudio("sounds/tom-4.mp3");
-        break;
-      case "j":
-        playAudio("sounds/snare.mp3");
-        break;
-      case "k":
-        playAudio("sounds/crash.mp3");
-        break;
-      case "l":
-        playAudio("sounds/kick.mp3");
-        break;
+  switch (key) {
+    case "a":
+      playAudio("sounds/tom-1.mp3");
+      break;
+    case "s":
+      playAudio("sounds/tom-2.mp3");
+      break;
+    case "d":
+      playAudio("sounds/tom-3.mp3");
+      break;
+    case "f":
+      playAudio("sounds/tom-4.mp3");
+      break;
+    case "g":
+      playAudio("sounds/snare.mp3");
+      break;
+    case "h":
+      playAudio("sounds/crash.mp3");
+      break;
+    case "j":
+      playAudio("sounds/kick.mp3");
+      break;
 
-      default:
-        console.log(key);
-    }
+    default:
+      console.log(key);
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var key = "." + currentKey;
+  var buttonPressd = document.querySelector(key);
+  buttonPressd.classList.add("pressed");
+  setTimeout(function () {
+    buttonPressd.classList.remove("pressed");
+  }, 100);
 }
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     makeSound(this.innerHTML);
+    buttonAnimation(this.innerHTML);
   });
 }
 
-document.addEventListener("keydown", function(event) {
-    makeSound(event.key)
-}
-)
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
